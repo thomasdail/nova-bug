fa58932982f2b3ba2617a4ac6ffb4cdb553cf25c
Thomas Dail <tdail30@gmail.com>
2025-08-14 16:44:26 -0400

    add additional columns

--- database/migrations/2025_08_14_201256_create_posts_table.php
+++ database/migrations/2025_08_14_201256_create_posts_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create("posts", function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("title");
+            $table->text("contents");
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists("posts");
     }
 };
--- database/migrations/2025_08_14_201339_create_comments_table.php
+++ database/migrations/2025_08_14_201339_create_comments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->id();
             $table->timestamps();
             $table->foreignIdFor(Post::class);
+            $table->text("content");
         });
     }
 
